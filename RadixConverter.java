6e7430e99721bb803a7f84513ccf71d6a3f23b58
Lucas Bevins <lucas173451@gmail.com>
2024-10-21 13:38:57 -0400

    Add files via upload

--- RadixConverter.java
+++ RadixConverter.java
@@ -11,7 +11,7 @@ public class RadixConverter {
 		int x = -1;
 		
 		while(x != 0) {
-			System.out.println("Note: Functionality is very basic at this point. As of now, only conversion of integers between two bases between 1 and 36, inclusive, is supported.");
+			System.out.println("Note: Functionality is very basic at this point. As of now, only conversion of integers between two bases between 2 and 36, inclusive, is supported.");
 			System.out.println("Please select an option:");
 			System.out.println("0. Quit");
 			System.out.println("1. Simple Base Conversion");
@@ -24,15 +24,15 @@ public class RadixConverter {
 				String input = s.next();
 				System.out.println("Please input the base of the integer:");
 				int iB = s.nextInt();
-				if(iB < 1 || iB > 36) {
-					System.out.println("Error: Base must be between 1 and 36, inclusive");
+				if(iB < 2 || iB > 36) {
+					System.out.println("Error: Base must be between 2 and 36, inclusive");
 					System.out.println("Please input the base of the integer:");
 					iB = s.nextInt();
 				}
 				System.out.println("Please input the base you want to convert the integer to:");
 				int oB = s.nextInt();
-				if(oB < 1 || oB > 36) {
-					System.out.println("Error: Base must be between 1 and 36, inclusive");
+				if(oB < 2 || oB > 36) {
+					System.out.println("Error: Base must be between 2 and 36, inclusive");
 					System.out.println("Please input the base you want to convert the integer to:");
 					oB = s.nextInt();
 				}
