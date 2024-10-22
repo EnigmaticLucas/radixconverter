d9547d9c3064cae27c4ac4e576f2edebd3ed4378
Lucas Bevins <lucas173451@gmail.com>
2024-10-22 12:52:16 -0400

    Add files via upload

--- RadixConverter.java
+++ RadixConverter.java
@@ -1,10 +1,13 @@
 package radixconverter;
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 public class RadixConverter {
 
 	public static void simpleConv(String a, int b, int c) {
-		System.out.println("Result: " + Integer.toString(Integer.parseInt(a, b), c));
+		System.out.println("Result: " + Integer.toString(Integer.parseInt(a, b), c) + "\u0007");
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
@@ -12,29 +15,39 @@ public class RadixConverter {
 		
 		while(x != 0) {
 			System.out.println("Note: Functionality is very basic at this point. As of now, only conversion of integers between two bases between 2 and 36, inclusive, is supported.");
-			System.out.println("Please select an option:");
+			System.out.println();
+			System.out.println("Menu:");
 			System.out.println("0. Quit");
 			System.out.println("1. Simple Base Conversion");
 			
+			System.out.print("Please select an option: ");
 			Scanner s = new Scanner(System.in);
 			x = s.nextInt();
 			
 			if(x == 1) {
-				System.out.println("Please input an integer:");
+				System.out.println();
+				System.out.print("Please input an integer: ");
 				String input = s.next();
-				System.out.println("Please input the base of the integer:");
+				System.out.println();
+				System.out.print("Please input the base of the integer: ");
 				int iB = s.nextInt();
+				System.out.println();
 				if(iB < 2 || iB > 36) {
 					System.out.println("Error: Base must be between 2 and 36, inclusive");
-					System.out.println("Please input the base of the integer:");
+					System.out.println();
+					System.out.print("Please input the base of the integer: ");
 					iB = s.nextInt();
+					System.out.println();
 				}
-				System.out.println("Please input the base you want to convert the integer to:");
+				System.out.print("Please input the base you want to convert the integer to: ");
 				int oB = s.nextInt();
+				System.out.println();
 				if(oB < 2 || oB > 36) {
 					System.out.println("Error: Base must be between 2 and 36, inclusive");
-					System.out.println("Please input the base you want to convert the integer to:");
+					System.out.println();
+					System.out.print("Please input the base you want to convert the integer to: ");
 					oB = s.nextInt();
+					System.out.println();
 				}
 				
 				simpleConv(input, iB, oB);
